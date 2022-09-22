08b01b00224d59bb1c5278c5367d1ab85211f8e8
Samuel Silva <samuel0100wanderson@gmail.com>
2022-09-22 19:27:56 -0300

    Bug encontrado, procurando solução

--- Criptografar & Descriptografar.py
+++ Criptografar & Descriptografar.py
@@ -5,14 +5,14 @@ escolha = int(input('Digite 1 se deseja criptografar e 2 se deseja descriptograf
 if escolha == 1:
     mensagem_original = input('Digite a senha a ser criptografada: ')
 
-    mensagem_criptografada = b64.b64encode(bytes(mensagem_original, 'UTF-8'))
+    mensagem_criptografada = b64.b64encode(bytes(mensagem_original, 'utf-8'))
     print('--------CRIPTOGRAFIA--------')
     mensagem_criptografada = str(mensagem_criptografada)
     mensagem_criptografada = mensagem_criptografada.strip("b'")
     print('Mensagem criptografada: {} '.format(mensagem_criptografada))
 elif escolha == 2:
-    print("Caso não digite uma criptografia válido obterá erro!")
-    mensagem_criptografada = str(input("Digite a senha a ser descriptografada: "))
+
+    mensagem_criptografada = input("Digite a senha a ser descriptografada: ")
     mensagem_original = "b'{}'".format(mensagem_criptografada)
     mensagem_original = b64.b64decode(mensagem_original)
     print('--------DESCRIPTOGRAFIA--------')
